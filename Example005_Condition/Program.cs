5d7fef611ddd91be8e6d8db4ffb30ae0cb275871
Dmitriy Pischik <pischik@bk.ru>
2023-01-14 18:20:56 +1000

    Added folder Example005_Condition

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("enter user name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower == "masha")
+{
+    Console.WriteLine("Wow, Masha is here!"); 
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.Write(username);
+}
